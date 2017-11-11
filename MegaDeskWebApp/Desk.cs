422fbcb29a3741098caedfa8a9a0e9afadd2c8b6
Clayton <har12115@byui.edu>
2017-11-11 14:12:35 -0700

    Added Objects, Edit Page, Delete Page Added DeskQuote and Desk Objects

---
+++ MegaDeskWebApp/Desk.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    public struct Desk
+    {
+
+        public enum Material
+        {
+            Laminate = 100,
+            Oak = 200,
+            Rosewood = 300,
+            Veneer = 125,
+            Pine = 50
+        };
+
+        public string DeskMaterial { get; set; }
+
+        public int Width { get; set; }
+
+        public int Depth { get; set; }
+
+        public int Drawers { get; set; }
+
+    }
---
+++ MegaDeskWebApp/DeskQuote.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    public class DeskQuote
+    {
+
+
+
+        const int basePrice = 200;
+        const int pricePerInchSquared = 1;
+        const int pricePerDrawer = 50;
+
+
+
+
+
+
+
+
+        public string CustomerName { get; set; }
+
+        public int RushOrder { get; set; }
+
+        public DateTime QuoteDate { get; set; }
+
+        public int QuotePrice { get; set; }
+
+        public Desk Desk { get; set; }
+
+       
+
+        public DeskQuote(Desk desk)
+        {
+            this.Desk = desk;
+
+        }
+
+        public int DeskSize()
+        {
+            int size = Desk.Width * Desk.Depth;
+            return size;
+        }
+
+        public int GetQuote()
+        {
+            int drawers = Desk.Drawers;
+            int totalPrice;
+            int size = DeskSize();
+            string material = Desk.DeskMaterial;
+            int materialPrice = 0;
+            int rushPrice;
+            int sizePrice;
+
+
+            if (size > 1000)
+            {
+                sizePrice = (size - 1000) * pricePerInchSquared;
+            }
+            else
+            {
+                sizePrice = 0;
+            }
+
+            switch (material)
+            {
+                case "Laminate":
+                    materialPrice = (int)Desk.Material.Laminate;
+                    break;
+                case "Oak":
+                    materialPrice = (int)Desk.Material.Oak;
+                    break;
+                case "Rosewood":
+                    materialPrice = (int)Desk.Material.Rosewood;
+                    break;
+                case "Veneer":
+                    materialPrice = (int)Desk.Material.Veneer;
+                    break;
+                case "Pine":
+                    materialPrice = (int)Desk.Material.Pine;
+                    break;
+            }
+
+            switch (RushOrder)
+            {
+                case 3:
+                    if (size >= 1000 || size <= 2000)
+                    {
+                        rushPrice = 70;
+                    }
+                    else if (size > 2000)
+                    {
+                        rushPrice = 80;
+                    }
+                    else
+                    {
+                        rushPrice = 60;
+                    }
+                    break;
+                case 5:
+                    if (size >= 1000 || size <= 2000)
+                    {
+                        rushPrice = 50;
+                    }
+                    else if (size > 2000)
+                    {
+                        rushPrice = 60;
+                    }
+                    else
+                    {
+                        rushPrice = 40;
+                    }
+                    break;
+                case 7:
+                    if (size >= 1000 || size <= 2000)
+                    {
+                        rushPrice = 35;
+                    }
+                    else if (size > 2000)
+                    {
+                        rushPrice = 40;
+                    }
+                    else
+                    {
+                        rushPrice = 30;
+                    }
+                    break;
+                default:
+                    rushPrice = 0;
+                    break;
+            }
+
+
+
+            totalPrice = basePrice + sizePrice + (pricePerDrawer * drawers) + materialPrice + rushPrice;
+
+            return totalPrice;
+        }
+
+
+    }
